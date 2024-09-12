238754ef9e0a0e1a9c451fd0af3599792acec320
Robert-J-WANG <robin.nz420@gmail.com>
2024-09-12 23:52:39 +1200

    update base path

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { join } from "node:path";
+import { BASE_PATH } from "./src/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/myPage/",
+  base: BASE_PATH,
   // 配置@路径, 应用组件时使用@代替./src/
   resolve: {
     alias: {
