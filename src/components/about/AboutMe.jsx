c8b0ec47ec399ffe8ebfd83d743ad22e5d4e31ef
Robert-J-WANG <robin.nz420@gmail.com>
2024-09-14 14:41:37 +1200

    update aboutme

--- src/components/about/AboutMe.jsx
+++ src/components/about/AboutMe.jsx
@@ -7,7 +7,7 @@ export default function AboutMe() {
     <div className="flex flex-col items-center justify-center w-full h-full gap-4 md:gap-12 md:flex-row">
       {/* left part */}
       <div className="flex items-center justify-center w-full h-full md:justify-end basis-1/2">
-        <div className="relative flex items-center justify-center w-[339px] h-[240px] overflow-hidden md:w-[226px] md:h-[320px] lg:w-[452px] lg:h-[320px] rounded-xl">
+        <div className="relative flex items-center justify-center w-[340px] h-[240px] lg:w-[408px] lg:h-[288px] overflow-hidden rounded-xl">
           <span className="absolute w-[700px] h-[700px] bg-gradient-to-br from-mainColor to-black animate-spin-slow"></span>
 
           {/* 使用 require() 动态加载图片 */}
@@ -19,9 +19,9 @@ export default function AboutMe() {
       </div>
 
       {/* right part */}
-      <div className="flex flex-col items-center justify-center w-full gap-4 md:gap-8 lg:gap-12 basis-1/2 md:items-start">
+      <div className="flex flex-col items-center justify-center w-full gap-4 md:gap-6 lg:gap-8 xl:gap-10 basis-1/2 md:items-start">
         {/* top title*/}
-        <div className="hidden text-xl md:text-2xl lg:text-3xl md:block">
+        <div className="hidden md:text-xl lg:text-2xl xl:text-3xl md:block">
           <span className="inline-block">Hi, I am&nbsp;</span>
           <span className="inline-block text-mainColor"> Robert</span>
         </div>
@@ -30,7 +30,7 @@ export default function AboutMe() {
           {infoData.map((item) => (
             <li
               key={item.id}
-              className="flex items-center justify-start gap-2 md:text-lg lg:text-xl text-ellipsis"
+              className="flex items-center justify-start gap-2 md:text-base lg:text-lg xl:text-xl text-ellipsis"
             >
               {/* dots */}
               <span className="inline-block w-2 h-2 rounded-full bg-mainColor"></span>
@@ -46,9 +46,11 @@ export default function AboutMe() {
         <a
           href="/resume.pdf"
           target="_blank"
-          className="flex items-center justify-center py-1 text-sm uppercase duration-500 border rounded w-44 sm:text-base text-mainColor bg-mainColor20 border-mainColor hover:bg-mainColor hover:text-bgColor "
+          className="flex items-center justify-center duration-500 border rounded text-mainColor bg-mainColor20 border-mainColor hover:bg-mainColor hover:text-bgColor "
         >
-          <span>Download Resume</span>
+          <span className="px-2 py-1 md:text-base lg:text-lg xl:text-xl">
+            Download Resume
+          </span>
         </a>
       </div>
     </div>
