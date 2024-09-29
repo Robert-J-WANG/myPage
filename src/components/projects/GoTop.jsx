e979a5959a2b21d6794c0be81d35c7ceebaff7ff
Robert-J-WANG <robin.nz420@gmail.com>
2024-09-29 20:04:37 +1300

    update GoTop

--- src/components/projects/GoTop.jsx
+++ src/components/projects/GoTop.jsx
@@ -3,12 +3,26 @@ import React from "react";
 export default function GoTop() {
   return (
     <button
-      className="btn btn-active btn-neutral bg-subBgColor text-mainColor "
+      className="p-2 rounded-full bg-subBgColor"
       onClick={() => {
         scrollTo({ top: 0, behavior: "smooth" });
       }}
     >
-      Go Top
+      <svg
+        width="40px"
+        height="40px"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect width="24" height="24" fill="none" />
+        <path
+          d="M7 14.5L12 9.5L17 14.5"
+          stroke="#9feaad"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
     </button>
   );
 }
